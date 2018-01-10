80ae5ef7d4b64d0b104ea92a68ca4acfedf59c8d
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-01-09 21:08:31 -0800

    task3

--- rosalind/src/main/scala/tasks/Task3.scala
+++ rosalind/src/main/scala/tasks/Task3.scala
@@ -8,6 +8,6 @@ object Task3 extends Solver {
   override def solve(reader: Iterator[String]): Any = {
     val seq = reader.trimmedLine
     val rule = Map('A'->'T','T'->'A','C'->'G','G'->'C')
-    seq.map(a => rule(a))
+    seq.map(a => rule(a)).reverse.toString
   }
 }

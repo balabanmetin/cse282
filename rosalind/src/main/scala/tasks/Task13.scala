264f9085bb31f4c31e6e3cb52b9db4c922231e77
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-01-10 03:41:33 -0800

    task12

---
+++ rosalind/src/main/scala/tasks/Task13.scala
@@ -0,0 +1,15 @@
+package tasks
+
+import tools.Solver
+import Task9._
+
+object Task13 extends Solver {
+
+  import Solver._
+
+  override def solve(reader: Iterator[String]): Any = {
+    val seq = reader.trimmedLine
+    val k = reader.nextInt
+    generateDBall(seq,k).mkString("\n")
+  }
+}

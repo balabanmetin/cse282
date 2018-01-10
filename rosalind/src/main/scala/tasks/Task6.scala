663387b3d25ba031cd4cc0be33527b83b98bd093
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-01-10 00:06:18 -0800

    task6

--- rosalind/src/main/scala/tasks/Task6.scala
+++ rosalind/src/main/scala/tasks/Task6.scala
@@ -8,12 +8,14 @@ object Task6 extends Solver {
 
   override def solve(reader: Iterator[String]): Any = {
     val seq = reader.trimmedLine
-    val indexed = seq.foldLeft((List(0), 0))((acc, c) =>
+    val indexed = seq.foldLeft((List(0)))((acc, c) =>
       c match {
-        case 'C' => (acc._1 :+ (acc._2 - 1), acc._2 - 1)
-        case 'G' => (acc._1 :+ (acc._2 + 1), acc._2 + 1)
-        case _ => (acc._1 :+ acc._2, acc._2)
-      })._1.zipWithIndex
-    indexed.filter(v => v._1 == indexed.min._1).map(_._2).mkString(" ")
+        case 'C' => (acc.head - 1)  :: acc
+        case 'G' => (acc.head + 1) :: acc
+        case _ => acc.head :: acc
+      }).reverse.zipWithIndex
+    println("Checkpoint")
+    val m = indexed.min._1
+    indexed.filter(v => v._1 == m).map(_._2).mkString(" ")
   }
 }

c5a3efdc3e1845e65d228fc58fc6a08ea4d2a8b7
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-01-18 15:48:34 -0800

    Task19

--- rosalind/src/main/scala/tasks/Task18.scala
+++ rosalind/src/main/scala/tasks/Task18.scala
@@ -12,13 +12,13 @@ object Task18 extends Solver {
   def formProfileWithPrior(motif: Array[String]): Array[Array[Double]] = {
     val trans = motif.map(_.toList).toList.transpose // ["ABC","DEF"] -> ["AD","BE","CF"]
     val nucProf = trans.map(_.groupBy(identity).mapValues(_.length))
-    val transposedProf = nucProf.map(prof => List('A','C','G','T').map(c => (1.0 + prof.getOrElse(c,0)*1.0)/(motif.length + 4)))
+    val transposedProf = nucProf.map(prof => List('A', 'C', 'G', 'T').map(c => (1.0 + prof.getOrElse(c, 0) * 1.0) / (motif.length + 4)))
     transposedProf.transpose.map(_.toArray).toArray
   }
 
   def greedyMotifSearchWithPrior(dna: Array[String], k: Int, t: Int): Array[String] = {
-    val allMotifsFound = dna(0).sliding(k).map{case s =>
-      dna.tail.foldLeft(Array(s)){ case (motif, text) =>
+    val allMotifsFound = dna(0).sliding(k).map { case s =>
+      dna.tail.foldLeft(Array(s)) { case (motif, text) =>
         motif :+ profileMostProbable(text, k, formProfileWithPrior(motif))
       }
     }
---
+++ rosalind/src/main/scala/tasks/Task19.scala
@@ -0,0 +1,30 @@
+package tasks
+
+import tasks.Task16._
+import tasks.Task17._
+import tasks.Task18._
+import tools.Solver
+import scala.util.Random
+
+
+object Task19 extends Solver {
+
+  import Solver._
+
+  def climb(motif: Array[String], dna: Array[String], k: Int): Array[String] = {
+    val profile = formProfileWithPrior(motif)
+    val motifnew = dna.map(profileMostProbable(_, k, profile))
+    if (score(motifnew) < score(motif)) climb(motifnew, dna, k) else motif
+  }
+
+  def randomizedMotifSearch(dna: Array[String], k: Int, t: Int): Array[String] = {
+    val initMotif = dna.map(text => Random.shuffle(text.sliding(k).toList).head)
+    climb(initMotif, dna, k)
+  }
+
+  override def solve(reader: Iterator[String]): Any = {
+    val Array(k, t) = reader.nextIntArray
+    val dna = reader.map(_.trim).toArray
+    (for (i <- 0 until 1000) yield randomizedMotifSearch(dna, k, t)).minBy(score).mkString("\n")
+  }
+}

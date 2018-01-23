8e4506f6a8fe80aabac53d9e66ca588e2610b5d6
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-01-23 10:44:34 -0800

    Task 24

---
+++ rosalind/src/main/scala/tasks/Task24.scala
@@ -0,0 +1,18 @@
+package tasks
+
+import tools.Solver
+
+object Task24 extends Solver {
+
+  import Solver._
+
+  // Find the string spelled by a genome path.
+  def reconstructFromGenomePath(path: List[String]): String = {
+    path.head ++ path.tail.map(_.last)
+  }
+
+  override def solve(reader: Iterator[String]): Any = {
+    val dna = reader.map(_.trim).toList
+    reconstructFromGenomePath(dna)
+  }
+}

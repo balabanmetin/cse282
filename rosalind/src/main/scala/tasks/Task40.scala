816bffed3aef2c938c2998393732c65a6c58b97e
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-01-31 00:52:01 -0800

    Task 40

--- rosalind/src/main/scala/tasks/Task40.scala
+++ rosalind/src/main/scala/tasks/Task40.scala
@@ -2,29 +2,30 @@ package tasks
 
 import tools.{Memo, Solver}
 
-object Task39 extends Solver {
+object Task40 extends Solver {
 
   import Solver._
 
   override def solve(reader: Iterator[String]): Any = {
     val seq1 = reader.trimmedLine
     val seq2 = reader.trimmedLine
-    longestCommonSubsequence(seq1, seq2)
+    editDistance(seq1, seq2)
   }
 
-  // A longest common subsequence of these strings.
-  def longestCommonSubsequence(seq1: String, seq2: String): String = {
+  // Find the edit distance between two strings.
+  def editDistance(seq1: String, seq2: String): Int = {
     type Input = (List[Char], List[Char])
     type Cache = (Int, Int)
-    type Output = List[Char]
+    type Output = Int
     type DP = Memo[Input, Cache, Output]
     implicit def ev(key: Input): Cache = (key._1.length, key._2.length)
 
     lazy val f: DP = Memo {
-      case (Nil, _) | (_, Nil) => Nil
-      case (a :: as, b :: bs) if a == b => a :: f(as, bs)
-      case (a, b) => List(f(a, b.tail), f(a.tail, b)).maxBy(_.length)
+      case (a, Nil) => a.length
+      case (Nil, b) => b.length
+      case (a :: as, b :: bs) if a == b => f(as, bs)
+      case (a, b) => List(f(a, b.tail), f(a.tail, b), f(a.tail, b.tail)).min + 1
     }
-    f(seq1.toList, seq2.toList).mkString("")
+    f(seq1.toList, seq2.toList)
   }
 }

cb1c6976f1c0a9061232513c4bc90b1cad96f6da
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-02-04 14:56:15 -0800

    Task 41

--- rosalind/src/main/scala/tasks/Task41.scala
+++ rosalind/src/main/scala/tasks/Task41.scala
@@ -19,51 +19,51 @@ object Task41 extends Solver {
       val value = keys.zip(line.tail.map(_.toInt)).toMap
       key -> value
     }).toMap
-    val alignment = globalAlignment(seq1, seq2, scoringMap)
+    val alignment = fastAlign(seq1, seq2, scoringMap)
     alignment.score.toString ++ "\n" ++ alignment.seq1 ++ "\n" ++ alignment.seq2
   }
 
 
   case class Alignment(score: Int, seq1: String, seq2: String)
 
+
   // Find the highest-scoring alignment between two strings using a scoring matrix.
-  def globalAlignment(seq1: String, seq2: String,
+  def fastAlign(seq1: String, seq2: String,
                       scoringMap: Map[Char, Map[Char, Int]],
                       indelPenalty: Int = -5): Alignment = {
-    type Input = (List[Char], List[Char])
-    type Cache = (Int, Int)
-    type Output = Int
-    type DP = Memo[Input, Cache, Output]
-    implicit def ev(key: Input): Cache = (key._1.length, key._2.length)
+    val dp: Array[Array[Int]] = Array.ofDim[Int](seq1.length + 1, seq2.length + 1)
+    for(i <- seq1.indices)
+      dp(i)(0) = i * indelPenalty
+    for(i <- seq2.indices)
+      dp(0)(i) = i * indelPenalty
+    for(i <- 1 to seq1.length)
+      for (j <- 1 to seq2.length)
+        dp(i)(j) = List(dp(i)(j - 1) + indelPenalty,
+                        dp(i - 1)(j) + indelPenalty,
+                        dp(i - 1)(j - 1) + scoringMap(seq1(i - 1))(seq2(j - 1))).max
 
-    lazy val f: DP = Memo {
-      case (a, Nil) => a.length * indelPenalty
-      case (Nil, b) => b.length * indelPenalty
-      case (a, b) => List(f(a, b.tail) + indelPenalty,
-                          f(a.tail, b) + indelPenalty,
-                          f(a.tail, b.tail) + scoringMap(a.head)(b.head)).max
-    }
-    val opt = f(seq1.toList, seq2.toList)
+    val opt = dp(seq1.length)(seq2.length)
 
     def prepend(chars: (Char, Char), seqs: (List[Char], List[Char])): (List[Char], List[Char]) =
       (chars._1 :: seqs._1, chars._2 :: seqs._2)
 
-    def backTrack(seq1: List[Char], seq2: List[Char], f: DP): (List[Char], List[Char]) = {
+    def backTrack(seq1: List[Char], seq2: List[Char], dp: Array[Array[Int]]): (List[Char], List[Char]) = {
       (seq1, seq2) match {
         case (Nil, Nil) => (Nil, Nil)
-        case (Nil, b :: bs) => prepend(('-', b), backTrack(Nil, bs, f))
-        case (a :: as, Nil) => prepend((a, '-'), backTrack(as, Nil, f))
+        case (Nil, b :: bs) => prepend(('-', b), backTrack(Nil, bs, dp))
+        case (a :: as, Nil) => prepend((a, '-'), backTrack(as, Nil, dp))
         case (a, b) =>
-          if (f(a, b) == f(a, b.tail) + indelPenalty)
-            prepend(('-', b.head), backTrack(a, b.tail, f))
-          else if (f(a, b) == f(a.tail, b) + indelPenalty)
-            prepend((a.head, '-'), backTrack(a.tail, b, f))
+          if (dp(a.length)(b.length) == dp(a.length)(b.length - 1) + indelPenalty)
+            prepend(('-', b.head), backTrack(a, b.tail, dp))
+          else if (dp(a.length)(b.length) == dp(a.length - 1)(b.length) + indelPenalty)
+            prepend((a.head, '-'), backTrack(a.tail, b, dp))
           else
-            prepend((a.head, b.head), backTrack(a.tail, b.tail, f))
+            prepend((a.head, b.head), backTrack(a.tail, b.tail, dp))
       }
     }
 
-    val alignment: (List[Char], List[Char]) = backTrack(seq1.toList, seq2.toList, f)
+    val alignment: (List[Char], List[Char]) = backTrack(seq1.toList, seq2.toList, dp)
     Alignment(opt, alignment._1.mkString(""), alignment._2.mkString(""))
   }
+
 }
